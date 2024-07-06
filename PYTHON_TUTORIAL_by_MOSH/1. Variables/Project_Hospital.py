477cc4531de11824177de095049bb01d48ae6ccb
Al-Amin Kabir <alaminkabirdip@gmail.com>
2024-07-06 13:40:03 +0600

    New added on Variable

---
+++ PYTHON_TUTORIAL_by_MOSH/1. Variables/Project_Hospital.py
@@ -0,0 +1,5 @@
+#We check in a patient named John Smith.
+#He's 20 years old and is a new patient.
+Patient_Name = 'John Smith'
+Age = 20
+New = True 
---
+++ PYTHON_TUTORIAL_by_MOSH/1. Variables/Python Executes Line by Line.py
@@ -0,0 +1,7 @@
+Jahidul_Islam_Sir=10
+Jahidul_Islam_Sir=20
+print(Jahidul_Islam_Sir)
+#Here...Jahidul_Islam_Sir is a variable and Python executes line by line.
+#So, when Python will get 10 stored in the variable, it will store. 
+#But in the next line, it will recieve 20 and store it. 
+#Finally we can see 20 after printing the variable.
