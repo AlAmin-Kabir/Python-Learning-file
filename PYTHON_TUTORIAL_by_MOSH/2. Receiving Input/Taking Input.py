01ea7c7581e7ef361c072fdfeb7995d142fc9558
Al-Amin Kabir <alaminkabirdip@gmail.com>
2024-07-06 14:53:16 +0600

    New at Input file

--- PYTHON_TUTORIAL_by_MOSH/2. Receiving Input/Taking Input.py
+++ PYTHON_TUTORIAL_by_MOSH/2. Receiving Input/Taking Input.py
@@ -1,7 +1,8 @@
 name = input("Enter your name: ")
 age = input("Enter your age: ")
 gpa = input("Enter you CGPA: ")
-
+#এখানে আমাদের input ফাংশনটা একটা In Built ফাংশন। 
+#name, age, gpa ভ্যারিয়েবলের ভেতরে input ফাংশনের ভেতর একটা করে string প্যারামিটার   দিয়েছি। 
 print("Student Information")
 print("-------------------")
 print("Name: "+name)
