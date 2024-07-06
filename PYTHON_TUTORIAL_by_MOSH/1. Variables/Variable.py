671f7799a681c97b5209a0c2526d48867c2eb30f
Al-Amin Kabir <alaminkabirdip@gmail.com>
2024-07-06 13:21:52 +0600

    A file in Variable added.

---
+++ PYTHON_TUTORIAL_by_MOSH/1. Variables/Variable.py
@@ -0,0 +1,9 @@
+#Checking variables by Jannat
+Jannat = 10 #This is an integer
+print(Jannat)
+Jannat = 10.01 #This is a float
+print(Jannat)
+Jannat = 'I am Jannat.' #This is a string
+print(Jannat)
+Jannat = True
+print(Jannat)
