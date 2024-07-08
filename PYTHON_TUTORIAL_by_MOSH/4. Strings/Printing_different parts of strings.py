280f6cbbf70758bad84ffe3dd98a7fcdf787ecb4
Al-Amin Kabir <alaminkabirdip@gmail.com>
2024-07-08 13:21:49 +0600

    On strings.

---
+++ PYTHON_TUTORIAL_by_MOSH/4. Strings/Printing_different parts of strings.py
@@ -0,0 +1,14 @@
+course = 'Python for Beginners.'
+#printing 1st letter.
+print(course[0])
+#printing last letter.
+print(course[-1])
+#printing an specific point.
+print(course[0:6])
+#printing from an specific point.(from the 1st)
+print(course[7:])
+#printing from an specific point.(from the last)
+print(course[:6])
+#printing All letters.
+print(course)
+print(course[:]) 
