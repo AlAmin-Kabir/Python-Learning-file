c8f08ad54fe0350aee2c0a759f8b36f202c72b67
Al-Amin Kabir <alaminkabirdip@gmail.com>
2024-07-09 22:46:01 +0600

    Math

--- PYTHON_TUTORIAL_by_MOSH/8. Operator Precedence/Operator_Precedence.py
+++ PYTHON_TUTORIAL_by_MOSH/8. Operator Precedence/Operator_Precedence.py
@@ -1,7 +1,8 @@
 x = 10+3*2**2
 '''
-1. Exponentiation 2**3
-2. Multiplication or division
-3. Addition or subtraction
+1. Parenthesis()
+2. Exponentiation 2**3
+3. Multiplication or division
+4. Addition or subtraction
 '''
 print(x)
---
+++ PYTHON_TUTORIAL_by_MOSH/9. Math Functions/Importing_MathFile.py
@@ -0,0 +1,4 @@
+import math
+print(math.ceil(2.9))
+print(math.floor(2.9))
+#Check Python 3 Math Module on Google for more
