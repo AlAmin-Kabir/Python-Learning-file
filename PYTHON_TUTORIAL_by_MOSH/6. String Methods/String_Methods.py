3e62ca429caeaa8cba45230684103c775bb9b4f7
Al-Amin Kabir <alaminkabirdip@gmail.com>
2024-07-09 13:41:53 +0600

    Recent on Arithmatic Operator

---
+++ PYTHON_TUTORIAL_by_MOSH/6. String Methods/String_Methods.py
@@ -0,0 +1,12 @@
+Course= 'Python for Beginners.'
+#Checking Characters length
+print(len(Course))
+#Converting Upper Case
+print(Course.upper())
+#Converting Lower Case
+print(Course.lower())
+#Finding a letter in String
+print(Course.find('t'))
+#Replacing letters
+print(Course.replace('P','Non Venomous P'))
+print('N' in Course)
---
+++ PYTHON_TUTORIAL_by_MOSH/7. Arithmetic Operations/Arithmatic Operators.py
@@ -0,0 +1,20 @@
+a= 10
+b=3
+#+(Addition)
+print(a+b)
+#-(Subtraction)
+print(a-b)
+#*(Multiplication) 
+print(a*b)
+#/ (Division)
+    #/(Float)
+print(a/b)
+    #/(int)
+print(a//b)
+#%(Remainder)
+print(a%b)
+#Power/Exponents
+print(a**b)
+#Assignment
+a+=3
+print(a)
