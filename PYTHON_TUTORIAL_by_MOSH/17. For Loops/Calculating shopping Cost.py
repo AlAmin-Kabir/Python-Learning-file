d4651425e0a3c28cf3e144e5fa97a512c90cba84
Al-Amin Kabir <alaminkabirdip@gmail.com>
2024-07-16 09:59:04 +0600

    Project on for

---
+++ PYTHON_TUTORIAL_by_MOSH/17. For Loops/Calculating shopping Cost.py
@@ -0,0 +1,8 @@
+'''
+In this project, calculate all the costs in the shopping.
+'''
+shopping_list = [10,15,12,23]
+total = 0
+for list in shopping_list:
+    total += list
+print(f'Total price is : {total}')
