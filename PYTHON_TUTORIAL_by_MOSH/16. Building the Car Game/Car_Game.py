d0704c5c9d5d4af5c5ae001e900158dd21628e04
Al-Amin Kabir <alaminkabirdip@gmail.com>
2024-07-16 06:06:15 +0600

    Updated.

--- PYTHON_TUTORIAL_by_MOSH/16. Building the Car Game/Car_Game.py
+++ PYTHON_TUTORIAL_by_MOSH/16. Building the Car Game/Car_Game.py
@@ -2,17 +2,21 @@
 This is a car building game.
 Here we will run car
 '''
-a=0
+s=0
 while True:
     command = input('>').lower()
     if command == 'start':
-        if(a == 0):
+        if(s == 0):
             print('Car started.')
-            a+=1
+            s+=1
         else:
             print("Car is already started.")
     elif command == 'stop':
-        print('Car stopped.')
+        if(s == 1):
+            print('Car stopped.')
+            s-=1
+        elif(s==0):
+            print('Car is already stopped.')
     elif command == 'quit':
         print('Quitting...')
         break
