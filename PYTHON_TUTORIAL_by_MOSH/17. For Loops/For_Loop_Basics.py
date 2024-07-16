73e06876093b685520d5e7aa02291061b807697c
Al-Amin Kabir <alaminkabirdip@gmail.com>
2024-07-16 09:49:50 +0600

    Range function

--- PYTHON_TUTORIAL_by_MOSH/17. For Loops/For_Loop_Basics.py
+++ PYTHON_TUTORIAL_by_MOSH/17. For Loops/For_Loop_Basics.py
@@ -1,2 +1,7 @@
-for items in 'Python':
-    print(items)
+'''For loop declaration
+1. Format is-
+    for (loop_Variable) in (string/range) :
+'''
+a = ['Spicy','Sticky','Beaf Steak']
+for talika in a:
+    print(talika)
---
+++ PYTHON_TUTORIAL_by_MOSH/17. For Loops/Range_Function.py
@@ -0,0 +1,19 @@
+'''
+Here I am going to using the range function.
+Uses of range function-
+    1. For iterating large amount of numbers.
+Format of range function-
+    a) If wants to print from 1 to n... --- range(n+1)
+Example:
+for talika in range(10):
+    print(talika)
+
+    b) For printing from one number to another... --- range(m,n)
+Example:
+for talika in range(5,10):
+    print(talika)
+    b) For printing from one number to another...with a sequence... --- range(m,n,sequence)
+Example:
+for talika in range(5,10,2):
+    print(talika)
+'''
